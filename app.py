1861f10923c01f7b759a5379a4b68b2893eab1cd
Ian McClue <123007216+IanMcClue@users.noreply.github.com>
2024-05-25 13:09:35 +0100

    new code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import streamlit as st
 import base64
-from openai import OpenAI
+import openai
 
+# Set the OpenAI API key using st.secrets
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-client = OpenAI()
+client = openai.Client()
 
 st.set_page_config(
     page_title="Image to Text",
@@ -15,17 +17,14 @@ st.set_page_config(
 
 st.title("AI Image Description Generator 🤖✍️")
 
-
 def to_base64(uploaded_file):
     file_buffer = uploaded_file.read()
     b64 = base64.b64encode(file_buffer).decode()
     return f"data:image/png;base64,{b64}"
 
-
 with st.sidebar:
     st.title("Upload Your Images")
     st.session_state.images = st.file_uploader(label=" ", accept_multiple_files=True)
-
 
 def generate_df():
     current_df = pd.DataFrame(
@@ -48,7 +47,6 @@ def generate_df():
 
     st.session_state.df = new_df
 
-
 def render_df():
     st.data_editor(
         st.session_state.df,
@@ -66,7 +64,6 @@ def render_df():
         column_order=["image", "name", "description"],
         use_container_width=True,
     )
-
 
 def generate_description(image_base64):
     response = client.chat.completions.create(
@@ -88,33 +85,3 @@ def generate_description(image_base64):
         max_tokens=50,
     )
     return response.choices[0].message.content
-
-
-def update_df():
-    indexes = st.session_state.df[st.session_state.df["description"] == ""].index
-    for idx in indexes:
-        description = generate_description(st.session_state.df.loc[idx, "image"])
-        st.session_state.df.loc[idx, "description"] = description
-
-
-if st.session_state.images:
-    generate_df()
-
-    st.text_input("Prompt", value="What's in this image?", key="text_prompt")
-
-    col1, col2 = st.columns([1, 1])
-
-    with col1:
-        if st.button("Generate Image Descriptions", use_container_width=True):
-            update_df()
-    
-    with col2:    
-        st.download_button(
-                "Download descriptions as CSV",
-                st.session_state.df.drop(['image', "image_id"], axis=1).to_csv(index=False),
-                "descriptions.csv",
-                "text/csv",
-                use_container_width=True
-        )
-
-    render_df()
